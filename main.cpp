b7ffac04e5a98bdd2e785ed32366b94b81bb7bf3
trong <lehuutrong141@gmail.com>
2023-03-09 20:04:36 +0700

    push main

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@
 #include "CFolder.h"
 #include "Conio.h"
 #include "CFile.h"
+#include"BiosParameterBlock.h"
+#include"BiosParameterBlockMapper.h"
+#include"MFTEntry.h"
 using std::string, std::cin;
 
 void program(CItem* item)
 {
     if (item->type() == "CFolder") {
         CFolder* folder = dynamic_cast<CFolder*>(item);
+        cout << 1;
         int index = 2;
         while (true) {
             system("cls");
+          
             folder->show(index);
             unsigned char key = _getch();
             if (int(key) == ARROW) {
@@ -72,25 +77,58 @@ void program(CItem* item)
 int main(int argc, char** argv)
 {
    // cout << " 1";
-    BootSector* bootSector = getBootSector();
-    IValueMapper* mapper = new BootSectorMapper;
+  //  BootSector* bootSector = getBootSector();
+  //  IValueMapper* mapper = new BootSectorMapper;
 
-    map<string, int> bootSectorMapper = mapper->mapper(bootSector);
-    int point = bootSectorMapper["Sb"] + bootSectorMapper["Nf"] * bootSectorMapper["Sf"]; 
-    ReadDirectoryTable* reader = new ReadDirectoryTable();
+  //  map<string, int> bootSectorMapper = mapper->mapper(bootSector);
+  //  int point = bootSectorMapper["Sb"] + bootSectorMapper["Nf"] * bootSectorMapper["Sf"]; 
+  //  ReadDirectoryTable* reader = new ReadDirectoryTable();
 
-    DirectoryTable* directory = dynamic_cast<DirectoryTable*> (reader->Read(point));
-  //  directory->child();
+  //  DirectoryTable* directory = dynamic_cast<DirectoryTable*> (reader->Read(point));
+  ////  directory->child();
 
-    RootDirectoryTable* RDET = dynamic_cast<RootDirectoryTable*> (directory);
+  //  RootDirectoryTable* RDET = dynamic_cast<RootDirectoryTable*> (directory);
 
-    MainEntry* mainEntry = dynamic_cast<MainEntry*>(RDET->entrys()[0]);
-    int startCluster = bootSectorMapper["ClusterBeginRDET"];
-    string name = mainEntry->headName();
-    CFolder* folder = new CFolder(name, startCluster); // Disk
-    folder->init();
-    //cout << folder->name();
-    program(folder);
+  //  MainEntry* mainEntry = dynamic_cast<MainEntry*>(RDET->entrys()[0]);
+  //  int startCluster = bootSectorMapper["ClusterBeginRDET"];
+  //  string name = mainEntry->headName();
+  //  CFolder* folder = new CFolder(name, startCluster); // Disk
+  //  folder->init();
+  //  //cout << folder->name();
+  //  program(folder);
 
-    return 0;
+  //  return 0;
+    BYTE sector[512];
+    string** sectors = ReadSector(HARD_DISK, 0, sector);
+    BiosParameterBlock* BPB = new BiosParameterBlock(sectors);
+    BiosParameterBlockMapper* mapper = new BiosParameterBlockMapper;
+    map<string, int> mp =  mapper->mapper(BPB);
+   /* for (auto i : mp)
+    {
+        cout << i.first << " " << i.second<<endl;
+    }*/
+    long long start = mp["ClusterBeginMFT"] * mp["Sc"];
+    cout << start * 512 << endl;
+    start = (start * 512);
+   
+   // cout << start << " " << 1 << endl;
+    string** str = new string * [64];
+    for (int i = 0; i < 64; i++)
+    {
+        str[i] = new string[16];
+    }
+
+    string** temp = ReadSectorBigByte(HARD_DISK, start, sector);
+    for (int i = 0; i < 32; i++)
+    {
+        str[i] = temp[i]; 
+    }
+    temp = ReadSectorBigByte(HARD_DISK, (start+512), sector);
+    for (int i = 0; i < 32; i++)
+    {
+        str[i+32] = temp[i];
+    }
+    MFTEntry* entry = new MFTEntry(str);
+    cout<<entry->toString();
+   // BPB->toString();
 }
